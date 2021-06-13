219c43f52588ac3dc7b5133df0ff5e230ac53da2
SnehaAS-12 <85794682+SnehaAS-12@users.noreply.github.com>
2021-06-13 11:36:06 +0530

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,35 @@
+print("30 days 30 hour challenge")
+print('30 days 30 hour challenge')
+Hours = "thirty"
+print(Hours)
+Days = "Thirty days"
+print(Days[0])
+Challenge = "I will win"
+print(Challenge[7:10])
+Challenge = "I will win"
+print(len(Challenge))
+Challenge = "I will win"
+print(Challenge.lower())
+a = "30 Days"
+b = "30 hours"
+c = a + b
+print(c)
+a = "30 Days"
+b = "30 hours"
+c = a + " " + b
+print(c)
+text = "Thirty days and Thirty hours"
+x = text.casefold()
+print(x)
+text = "Thirty days and Thirty hours"
+x = text.capitalize()
+print(x)
+text = "Thirty days and Thirty hours"
+x = text.find("T")
+print(x)
+text = "Thirty days and Thirty hours"
+x = text.isalpha()
+print(x)
+text = "Thirty days and Thirty hours"
+x = text.isalnum()
+print(x)
